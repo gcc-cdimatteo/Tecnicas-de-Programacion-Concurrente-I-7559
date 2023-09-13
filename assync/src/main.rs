408bb6566e8765804abd7a69bc885be5c447dac2
Fran Orquera <franorquera@Frans-MacBook-Pro.local>
2023-09-12 22:51:43 -0300

    artist-top-10-tracks

--- assync/src/main.rs
+++ assync/src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
+use async_std::task;
+
 use rspotify::{
-    model::{AlbumId, Country, Market},
+    model::{PlaylistId, PlayableItem, ArtistId, FullTracks, Market},
     prelude::BaseClient,
-    scopes, AuthCodeSpotify, ClientCredsSpotify, Credentials, OAuth,
+    ClientCredsSpotify, Credentials,
 };
 
 #[tokio::main]
@@ -12,7 +15,63 @@ async fn main() {
     );
     let spotify = ClientCredsSpotify::new(creds);
     spotify.request_token().await.unwrap();
-    let birdy_uri = AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
-    let albums = spotify.album(birdy_uri, None).await;
-    println!("Response: {albums:#?}");
+
+    let playlist_uri = PlaylistId::from_id("1JkjsP4R7jTh6jPVzlsfP7").unwrap();
+    let playlist = spotify.playlist(playlist_uri, None, None).await.unwrap();
+    let mut artists_id: HashMap<ArtistId, u32> = HashMap::new();
+
+    for item in playlist.tracks.items.iter() {
+        let playable_item = item.clone().track.unwrap();
+        match playable_item {
+            PlayableItem::Track(val) => {
+                let full_track_artists = val.artists;
+                let artist_id = full_track_artists.first().unwrap().id.as_ref().unwrap();
+                if !artists_id.contains_key(artist_id) {
+                    artists_id.insert(artist_id.clone(), 0);
+                }
+            },
+            PlayableItem::Episode(_) =>  (),
+        }
+    }
+
+    let mut top_tracks: HashMap<ArtistId, FullTracks> = HashMap::new();
+
+    artists_id.into_iter().for_each(|id| {
+        let full_tracks = task::block_on(get_full_tracks_by_artist(&spotify, &id.0));
+        top_tracks.insert(id.0, full_tracks);
+    });
+
+
+    // let artists_id_copy = artists_id.clone();
+
+    // for (artist_id, _followers) in artists_id_copy.iter() {
+    //     // let artist_uri = ArtistId::from_id(artist_id.to_string());
+    //     let artist = spotify.artist(artist_id.clone()).await.unwrap();
+    //     let follows = artist.followers.total;
+    //     artists_id.insert(artist_id.clone(), follows);
+    //     println!("({:?},{:?})", artist_id,artists_id.get(artist_id));
+    // }
 }
+
+async fn get_full_tracks_by_artist(spotify: &ClientCredsSpotify, id: &ArtistId<'_>) -> FullTracks {
+    let result = FullTracks {
+        tracks: spotify.artist_top_tracks(id.clone(), Some(Market::Country(rspotify::model::Country::Argentina))).await.unwrap()
+    };
+    result
+}
+
+    // let artists_id_c = Arc::new(artists_id);
+
+    // thread::spawn(move || {
+    //     artists_id.into_iter().for_each(|id| {
+    //         let full_tracks = task::block_on(get_full_tracks_by_artist(&spotify, &id.0));
+    //         top_tracks.insert(id.0, full_tracks);
+    //     });
+    // });
+
+    // thread::spawn(move || {
+    //     artists_id.into_iter().for_each(|id| {
+    //         let full_tracks = task::block_on(get_full_tracks_by_artist(&spotify, &id.0));
+    //         top_tracks.insert(id.0, full_tracks);
+    //     });
+    // });
