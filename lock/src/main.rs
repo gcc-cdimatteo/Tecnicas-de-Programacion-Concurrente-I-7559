f4a380b8741384354270dff48464c5e82f750c0e
Carolina Di Matteo <cdimatteo@fi.uba.ar>
2023-09-20 12:54:22 -0300

    refactor

--- lock/src/main.rs
+++ lock/src/main.rs
@@ -1,3 +1,4 @@
+use core::time;
 // Realizar un pequeño idle game
 // Algunas entidades "extraerán" (generarán aleatoriamente) oro
 // Algunas entidades podrán convertir oro en recursos (a gusto)
@@ -6,8 +7,11 @@
 // Periódicamente se reporta por pantalla el nivel de recursos y oro
 use std::{
     sync::{Arc, RwLock},
-    thread::{self, JoinHandle},
+    thread::{self, sleep, JoinHandle},
 };
+
+const RANGE_START: i32 = 0;
+const RANGE_END: i32 = 30;
 
 struct Gold {
     amount: i64,
@@ -23,106 +27,92 @@ fn extract_gold(gold: Arc<RwLock<Gold>>) {
     // while end-start < 15: extrae oro;
 
     // let end;
+    println!("EXTRACT GOLD");
 }
-fn convert_gold(gold: Arc<RwLock<Gold>>, resource: Arc<RwLock<Resource>>) {}
-fn convert_resource(resource: Arc<RwLock<Resource>>, gold: Arc<RwLock<Gold>>) {}
-fn consume_resource(resource: Arc<RwLock<Resource>>) {}
+
+fn convert_gold(gold: Arc<RwLock<Gold>>, resource: Arc<RwLock<Resource>>) {
+    println!("CONVERT GOLD");
+}
+
+fn convert_resource(resource: Arc<RwLock<Resource>>, gold: Arc<RwLock<Gold>>) {
+    println!("CONVERT RESOURCE");
+}
+
+fn consume_resource(resource: Arc<RwLock<Resource>>) {
+    println!("CONSUME RESOURCE");
+}
 
 fn run_extractions(gold: Arc<RwLock<Gold>>) {
-    let mut extractions_handle = vec![];
-
-    for _i in 0..3 {
-        let gold_copy = gold.clone();
-        let handle = thread::spawn(move || extract_gold(gold_copy));
-        extractions_handle.push(handle);
-    }
-
-    for handle in extractions_handle {
-        handle.join().unwrap()
+    for _ in RANGE_START..RANGE_END {
+        extract_gold(gold.to_owned());
     }
 }
 
 fn run_convert_gold(gold: Arc<RwLock<Gold>>, resource: Arc<RwLock<Resource>>) {
-    let mut conversor_handle = vec![];
-
-    for _i in 0..3 {
-        let gold_copy = gold.clone();
-        let resource_copy = resource.clone();
-        let handle = thread::spawn(move || convert_gold(gold_copy, resource_copy));
-        conversor_handle.push(handle);
-    }
-
-    for handle in conversor_handle {
-        handle.join().unwrap()
+    for _ in RANGE_START..RANGE_END {
+        convert_gold(gold.to_owned(), resource.to_owned());
     }
 }
 
 fn run_convert_resource(resource: Arc<RwLock<Resource>>, gold: Arc<RwLock<Gold>>) {
-    let mut conversor_handle = vec![];
-
-    for _i in 0..3 {
-        let gold_copy = gold.clone();
-        let resource_copy = resource.clone();
-        let handle = thread::spawn(move || convert_resource(resource_copy, gold_copy));
-        conversor_handle.push(handle);
-    }
-
-    for handle in conversor_handle {
-        handle.join().unwrap()
+    for _ in RANGE_START..RANGE_END {
+        convert_resource(resource.to_owned(), gold.to_owned());
     }
 }
 
 fn run_consume_resource(resource: Arc<RwLock<Resource>>) {
-    let mut consumer_handle = vec![];
-
-    for _i in 0..3 {
-        let resource_copy = resource.clone();
-        let handle = thread::spawn(move || consume_resource(resource_copy));
-        consumer_handle.push(handle);
-    }
-
-    for handle in consumer_handle {
-        handle.join().unwrap()
+    for _ in RANGE_START..RANGE_END {
+        consume_resource(resource.to_owned());
     }
 }
 
-fn print_gold(gold: Arc<RwLock<Gold>>) {}
+fn print_gold(gold: Arc<RwLock<Gold>>) {
+    while true {
+        println!("PRINT GOLD");
+        sleep(time::Duration::from_secs(2));
+    }
+}
 
-fn print_resource(resource: Arc<RwLock<Resource>>) {}
+fn print_resource(resource: Arc<RwLock<Resource>>) {
+    while true {
+        println!("PRINT RESOURCE");
+        sleep(time::Duration::from_secs(2));
+    }
+}
 
 fn main() {
     let gold = Arc::new(RwLock::new(Gold { amount: 0 }));
     let resource = Arc::new(RwLock::new(Resource { amount: 0 }));
 
-    let mut process_handle = vec![];
+    let mut handler = vec![];
 
-    let g_c = gold.clone();
-    let t_re = thread::spawn(move || run_extractions(g_c));
-    process_handle.push(t_re);
+    let gold_clone = gold.clone();
+    let t = thread::spawn(move || run_extractions(gold_clone));
+    handler.push(t);
 
-    let g_c = gold.clone();
-    let r_c = resource.clone();
-    let t_cg = thread::spawn(move || run_convert_gold(g_c, r_c));
-    process_handle.push(t_cg);
+    let gold_clone = gold.clone();
+    let resource_clone = resource.clone();
+    let t = thread::spawn(move || run_convert_gold(gold_clone, resource_clone));
+    handler.push(t);
 
-    let g_c = gold.clone();
-    let r_c = resource.clone();
-    let t_cr = thread::spawn(move || run_convert_resource(r_c, g_c));
-    process_handle.push(t_cr);
+    let gold_clone = gold.clone();
+    let resource_clone = resource.clone();
+    let t = thread::spawn(move || run_convert_resource(resource_clone, gold_clone));
+    handler.push(t);
 
-    let r_c = resource.clone();
-    let t_cor = thread::spawn(move || run_consume_resource(r_c));
-    process_handle.push(t_cor);
+    let resource_clone = resource.clone();
+    let t = thread::spawn(move || run_consume_resource(resource_clone));
+    handler.push(t);
 
-    let g_c = gold.clone();
-    let t_pg = thread::spawn(move || print_gold(g_c));
-    process_handle.push(t_pg);
+    let gold_clone = gold.clone();
+    let t = thread::spawn(move || print_gold(gold_clone));
+    handler.push(t);
 
-    let r_c = resource.clone();
-    let t_pr = thread::spawn(move || print_resource(r_c));
-    process_handle.push(t_pr);
+    let resource_clone = resource.clone();
+    let t = thread::spawn(move || print_resource(resource_clone));
+    handler.push(t);
 
-    for h in process_handle {
+    for h in handler {
         h.join().unwrap()
     }
 }
