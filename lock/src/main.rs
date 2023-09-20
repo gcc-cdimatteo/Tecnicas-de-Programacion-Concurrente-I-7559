3ba1b3b9b5443677d673417f64dfa281a96d48b7
Carolina Di Matteo <cdimatteo@fi.uba.ar>
2023-09-20 19:18:25 -0300

    exercise finished

--- lock/src/main.rs
+++ lock/src/main.rs
@@ -6,40 +6,76 @@ use core::time;
 // Otras entidades podrán solamente consumir recursos
 // Periódicamente se reporta por pantalla el nivel de recursos y oro
 use std::{
-    sync::{Arc, RwLock},
-    thread::{self, sleep, JoinHandle},
+    sync::{Arc, Mutex},
+    thread::{self, sleep},
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 const RANGE_START: i32 = 0;
 const RANGE_END: i32 = 10;
+const MAX_DURATION: Duration = Duration::new(5, 0);
 
 struct Gold {
-    amount: RwLock<i64>,
+    amount: Mutex<i64>,
 }
 
 struct Resource {
-    amount: RwLock<i64>,
+    amount: Mutex<i64>,
 }
 
 fn extract_gold(gold: Arc<Gold>) {
-    // let start;
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 
-    // while end-start < 15: extrae oro;
-
-    // let end;
-    println!("EXTRACT GOLD");
+    while SystemTime::now().duration_since(UNIX_EPOCH).unwrap() - start < MAX_DURATION {
+        {
+            *gold.amount.lock().unwrap() += 2;
+        }
+    }
 }
 
 fn convert_gold(gold: Arc<Gold>, resource: Arc<Resource>) {
-    println!("CONVERT GOLD");
+    // 4 Gold --> 1 Resource
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+
+    while SystemTime::now().duration_since(UNIX_EPOCH).unwrap() - start < MAX_DURATION {
+        {
+            let mut gold_amount = *gold.amount.lock().unwrap();
+
+            if gold_amount >= 4 {
+                gold_amount -= 4;
+                *resource.amount.lock().unwrap() += 1;
+            }
+        }
+    }
 }
 
 fn convert_resource(resource: Arc<Resource>, gold: Arc<Gold>) {
-    println!("CONVERT RESOURCE");
+    // 2 Resources --> 1 Gold
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+
+    while SystemTime::now().duration_since(UNIX_EPOCH).unwrap() - start < MAX_DURATION {
+        {
+            let mut resource_amount = *resource.amount.lock().unwrap();
+
+            if resource_amount >= 2 {
+                resource_amount -= 2;
+                *gold.amount.lock().unwrap() += 1;
+            }
+        }
+    }
 }
 
 fn consume_resource(resource: Arc<Resource>) {
-    println!("CONSUME RESOURCE");
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+
+    while SystemTime::now().duration_since(UNIX_EPOCH).unwrap() - start < MAX_DURATION {
+        {
+            let mut resource_amount = *resource.amount.lock().unwrap();
+            if resource_amount >= 1 {
+                resource_amount -= 1;
+            }
+        }
+    }
 }
 
 fn run_extract_gold(gold: Arc<Gold>) {
@@ -93,25 +129,29 @@ fn run_consume_resource(resource: Arc<Resource>) {
 }
 
 fn print_gold(gold: Arc<Gold>) {
-    while true {
-        println!("PRINT GOLD: {}", gold.amount.read().unwrap());
-        sleep(time::Duration::from_secs(2));
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+
+    while SystemTime::now().duration_since(UNIX_EPOCH).unwrap() - start < 2 * MAX_DURATION {
+        println!("PRINT GOLD: {}", *gold.amount.lock().unwrap());
+        sleep(time::Duration::from_secs(1));
     }
 }
 
 fn print_resource(resource: Arc<Resource>) {
-    while true {
-        println!("PRINT RESOURCE: {}", resource.amount.read().unwrap());
-        sleep(time::Duration::from_secs(2));
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+
+    while SystemTime::now().duration_since(UNIX_EPOCH).unwrap() - start < 2 * MAX_DURATION {
+        println!("PRINT RESOURCES: {}", *resource.amount.lock().unwrap());
+        sleep(time::Duration::from_secs(1));
     }
 }
 
 fn main() {
     let gold = Arc::new(Gold {
-        amount: RwLock::new(0),
+        amount: Mutex::new(0),
     });
     let resource = Arc::new(Resource {
-        amount: RwLock::new(0),
+        amount: Mutex::new(0),
     });
 
     let mut handler = vec![];
